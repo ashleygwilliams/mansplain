6dc43a0f0784d56fc56eae216993f66bad0133b8
Ashley Williams <ashley@bocoup.com>
2015-10-28 18:30:59 -0400

    do not replace first match, but remove (#), fixes #4

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function mansplain(html) {
+  var first = true;
   var result = html.replace(/([-.a-z]+)\((\d)\)/g, function(match, p1, p2) {
+    if (first) {
+      first = false;
+      return match.replace(/\(\d\)/, '');
+    }
     var cmd = p1.replace(/npm-/, "");
     return '<a href="' + cmd + '">' + p1 + '</a>';
   });
