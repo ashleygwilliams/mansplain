916f2437e66f82ab44829e4d397ea3c4218ac6eb
Ashley Williams <ashley@bocoup.com>
2015-10-28 18:12:56 -0400

    match only, not any, npm-, fixes #2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function mansplain(html) {
   var result = html.replace(/([-.a-z]+)\((\d)\)/g, function(match, p1, p2) {
-    var cmd = p1.replace(/[npm-]+/, "");
+    var cmd = p1.replace(/npm-/, "");
     return '<a href="cli/' + cmd + '">' + p1 + '</a>';
   });
   return result;
