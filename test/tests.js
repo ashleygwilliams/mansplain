185af745441387bf17b499625ed3ebb085fddea0
Ashley Williams <ashley@bocoup.com>
2015-10-29 08:56:56 -0400

    swap chai for should, no mocha opts, update test to remove cli prefix

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,10 @@
-/* global describe, it */
-'use strict'
-
 const mansplain = require('../');
+const should = require('chai').should();
 
 describe('mansplain', function () {
   it('should convert an npm man link to an html anchor element', function () {
     const input = '<li>npm-cmd(9)</li>';
     const result = mansplain(input);
-    result.should.equal('<li><a href="cli/cmd">cmd</a></li>');
+    result.should.equal('<li><a href="cmd">cmd</a></li>');
   });
 });
