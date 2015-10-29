271ed01ec1d4e08064d4036df61008aa2f388da0
Ashley Williams <ashley@bocoup.com>
2015-10-29 17:08:34 -0400

    refactor to use prefixes hash

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-var FILES_DOCS = ["folders", "npmrc", "package.json"];
-var MISC_DOCS = ["coding-style", "config", "developers", "disputes", "faq", "orgs", "registry", "removing-npm", "scope", "scripts", "semver"];
-
 function mansplain(opts) {
   var input = opts.input;
   var skip = opts.skip;
-  var prefix = opts.prefix;
-  var npm_mode = opts.npm_mode;
-  if (!prefix) {
-    prefix = "";
-  } else {
-    prefix = '/' + prefix;
-  }
+  var prefix_hash = opts.prefix;
   var result = input.replace(/([-.a-z]+)\((\d)\)/g, function(match, p1, p2) {
     if (skip) {
       skip = false;
       return match.replace(/\(\d\)/, '');
     }
     var cmd = p1.replace(/npm-/, "");
-    if (npm_mode && FILES_DOCS.indexOf(cmd) !== -1) {
-      return '<a href="/files/' + cmd + '">' + p1 + '</a>';
-    } else if (npm_mode && MISC_DOCS.indexOf(cmd) !== -1) {
-      return '<a href="/misc/' + cmd + '">' + p1 + '</a>';
+    var prefix;
+    if (prefix_hash) {
+      prefix = '/' + prefix_hash[p2];
+    } else {
+      prefix = ""
     }
     return '<a href="' + prefix + '/' + cmd + '">' + p1 + '</a>';
   });
